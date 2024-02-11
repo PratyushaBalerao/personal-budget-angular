df6790754949b5de56c24bea0d21bdc142304505
PratyushaBalerao <157338579+PratyushaBalerao@users.noreply.github.com>
2024-02-11 10:37:35 -0500

    commit changes

--- server.js
+++ server.js
@@ -10,23 +10,50 @@ app.use(cors());
 const budget = {
     myBudget: [
         {
-            title: 'Eat out',
-            budget: 25
+            title: 'Electronics',
+            budget: 300
         },
         {
             title: 'Rent',
-            budget: 275
+            budget: 350
         },
         {
-            title: 'Grocery',
-            budget: 110
+            title: 'Market place',
+            budget: 90
+        },
+        {
+            title: 'Parking',
+            budget: 30
+        },
+        {
+            title: 'Cab',
+            budget: 250
+        },
+        {
+            title: 'Travel',
+            budget: 450
+        },
+        {
+            title: 'Other',
+            budget: 359
         },
     ]
 };
 
+const data = [
+    {"Framework": "Vue", "Stars": "166443", "Released": "2014"},
+    {"Framework": "React", "Stars": "150793", "Released": "2013"},
+    {"Framework": "Angular", "Stars": "62342", "Released": "2016"},
+    {"Framework": "Backbone", "Stars": "27647", "Released": "2010"},
+    {"Framework": "Ember", "Stars": "21471", "Released": "2011"},
+  ];
+
 
 app.get('/budget', (req, res) => {
     res.json(budget);
+});
+app.get('/data',(req,res) => {
+    res.json(data);
 });
 
 app.listen(port, () => {
